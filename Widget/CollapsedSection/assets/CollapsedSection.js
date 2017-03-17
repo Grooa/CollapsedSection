302b9830f8bffe27e98681517a9aa66a5ad284a3
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-03-17 21:18:23 +0100

    feat: Add custom labeling of button

---
+++ Widget/CollapsedSection/assets/CollapsedSection.js
@@ -0,0 +1,12 @@
+
+IpWidget_CollapsedSection = function() {
+	this.$widgetObject = null;
+
+	this.init = function($widgetObject, data) {
+		var item = $widgetObject.find('.masking-button');
+		item.click(function() {
+			var value = prompt('Enter the value');
+			$widgetObject.save({btnLabel: value}, true);
+		});
+	};
+}
---
+++ Widget/CollapsedSection/skin/default.php
@@ -0,0 +1,8 @@
+<div>
+	<button class="masking-button"><?=isset($btnLabel) ? $btnLabel : "Activate" ?></button>
+	<div class="hidden-data"></div>
+</div>
+
+
+<script type="text/javascript">
+</script>
